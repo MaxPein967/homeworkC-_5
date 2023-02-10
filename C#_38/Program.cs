c23df912161c24c288b3b0013ec4a91471e35ffd
Sergei Vasiliev <9670105747@mail.ru>
2023-02-10 10:05:01 +0700

    Add #38 real numbers max-min in mass

--- C#_38/Program.cs
+++ C#_38/Program.cs
@@ -3,3 +3,37 @@
 //элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+Console.Clear();
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] nums = new double[size];
+FillArrayRandomNumbers(nums);
+PrintArray(nums);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < nums.Length; x++)
+{
+    if (nums[x] > max) max = nums[x];
+    if (nums[x] < min) min = nums[x];
+}
+
+Console.WriteLine($"-> " + Math.Round(max - min, 2));
+
+void FillArrayRandomNumbers(double[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;// вещественные числа с плавающей запятой
+    }
+}
+void PrintArray(double[] nums)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.Write(nums[i] + " ");
+    }
+    Console.Write("]");
+}
