c6d64fc8476ecf3bfd165d7befa1c090751c9991
Sergei Vasiliev <9670105747@mail.ru>
2023-02-10 08:44:23 +0700

    Add #34 random mass %2

--- C#_34/Program.cs
+++ C#_34/Program.cs
@@ -4,3 +4,34 @@
 //в массиве.
 
 // [345, 897, 568, 234] -> 2
+
+Console.Clear();
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[size];
+FillArrayRandomNumbers(nums);
+PrintArray(nums);
+int count = 0;
+
+for (int x = 0; x < nums.Length; x++)
+    if (nums[x] % 2 == 0)
+        count++;
+
+Console.Write($" -> {count} ");
+
+void FillArrayRandomNumbers(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] nums)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.Write(nums[i] + ", ");
+    }
+    Console.Write("]");
+}
