ded66ba79615ca46212dc8a66f199d09be8855fc
Sergei Vasiliev <9670105747@mail.ru>
2023-02-10 09:05:21 +0700

    Add #36 sum odd numbers in mass

--- C#_36/Program.cs
+++ C#_36/Program.cs
@@ -4,3 +4,33 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[size];
+FillArrayRandomNumbers(nums);
+PrintArray(nums);
+int sum = 0;
+
+for (int x = 1; x < nums.Length; x += 2)
+    sum = sum + nums[x];
+
+Console.WriteLine($" ->  {sum}");
+
+void FillArrayRandomNumbers(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] nums)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.Write(nums[i] + ", ");
+    }
+    Console.Write("]");
+}
